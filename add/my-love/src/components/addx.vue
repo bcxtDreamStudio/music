2c7a68dab424354f69287b1310462c12d679e88a
bcxtDreamStudio <42442819+bcxtDreamStudio@users.noreply.github.com>
2018-08-17 23:23:24 +0800

    change demo1

---
+++ add/my-love/src/components/addx.vue
@@ -0,0 +1,20 @@
+<!--根标签<div>【不规定那一个标签】 就只能有一个存在-->
+<template>
+    <div>
+        内容....
+    </div>
+</template>
+<script>
+    //文件引入
+    import ""
+    //默认输出
+    export default {
+        data() {
+            return {}
+        }
+    }
+</script>
+<!--scoped:表示 样式只能 本文件中的html可用-->
+<style scoped>
+
+</style>
